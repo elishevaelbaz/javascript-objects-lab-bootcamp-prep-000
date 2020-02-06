26b7205aa160ec42f778cf1e83a32ffe1d361934
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-02-06 17:17:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 
-// function deleteFromObjectByKey{object, key){
+function deleteFromObjectByKey{object, key){
+  var newObj = Object.assign({}, obj)
+  delete newObj[key]
+  return newObj
+  
 
-// }
+}
 
 function destructivelyDeleteFromObjectByKey(object, key){
   delete object[key];
